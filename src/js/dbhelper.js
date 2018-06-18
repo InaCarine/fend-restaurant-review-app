795cbd53bad86a7704186ae27b5c741addfbd2db
Ina Carine <inakara89@gmail.com>
2018-06-18 20:01:43 +0100

    fix restaurants json link

--- src/js/dbhelper.js
+++ src/js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     //const port = 8887 // Change this to your server port
-    return '/dist/data/restaurants.json';
+    return '//inacarine.github.io/dist/data/restaurants.json';
   }
 
   /**
