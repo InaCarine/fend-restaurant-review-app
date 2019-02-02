8857c2c606d7817f9c96d21fa2cea17b074d256f
Ina Carine <inakara89@gmail.com>
2019-02-02 20:35:50 +0000

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -79,7 +79,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiZmVuZGluYSIsImEiOiJjamljNnk0d3owMXZ6M3BtejNkdnM0cjFoIn0.0V47V2imj03I6gY2TTBWng',
+    mapboxToken: 'TOKEN',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
